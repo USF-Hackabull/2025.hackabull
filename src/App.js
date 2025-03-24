731f94427b9a85d1978e3ce1d5cf35f80a04a9b3
viennguyen24 <viennguyen@usf.edu>
2025-03-24 13:52:44 -0400

    setup parallax landing and about section

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import './App.css'
-import { Navbar } from './components/navbar'
-import { Home } from './components/home'
-import { About } from './components/about'
-import { Tracks } from './components/tracks'
-import { Schedule } from './components/schedule'
-import { FAQ } from './components/faq'
-import { Sponsors } from './components/sponsors'
-import { Speaker } from './components/speaker'
-import { Backdrop } from './components/backdrop'
+import "./App.css";
+import { Navbar } from "./components/navbar";
+import { Home } from "./components/home";
+import { About } from "./components/about";
+import { Tracks } from "./components/tracks";
+import { Schedule } from "./components/schedule";
+import { FAQ } from "./components/faq";
+import { Sponsors } from "./components/sponsors";
+import { Speaker } from "./components/speaker";
+import { Backdrop } from "./components/backdrop";
+import { Billboard } from "./components/test";
 
 function App() {
   return (
     <>
-
-<a
+      <a
         id="mlh-trust-badge"
         style={{
-          display: 'block',
-          maxWidth: '100px',
-          minWidth: '60px',
-          position: 'absolute',
-          left: '10px',
-          top: '0',
-          width: '10%',
+          display: "block",
+          maxWidth: "100px",
+          minWidth: "60px",
+          position: "absolute",
+          left: "10px",
+          top: "0",
+          width: "10%",
           zIndex: 10000,
         }}
         href="https://mlh.io/na?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2025-season&utm_content=white"
@@ -32,47 +32,45 @@ function App() {
         <img
           src="https://s3.amazonaws.com/logged-assets/trust-badge/2025/mlh-trust-badge-2025-white.svg"
           alt="Major League Hacking 2025 Hackathon Season"
-          style={{ width: '100%' }}
+          style={{ width: "100%" }}
         />
       </a>
-      
-      <Navbar />
 
-      
+      <Navbar />
 
       <Home />
 
-      <Backdrop>
-        <section id='about'>
-          <About />
-        </section>
+      <section id="about">
+        <About />
+      </section>
 
-        <section id='tracks'>
-          <Tracks />
-        </section>
+      <section id="tracks">
+        <Tracks />
+      </section>
 
-        <section id='speaker'>
-          <Speaker />
-        </section>
+      <section id="speaker">
+        <Speaker />
+      </section>
 
-        <section id='schedule'>
-          <Schedule />
-        </section>
+      <section id="schedule">
+        <Schedule />
+      </section>
 
-        <section id='faq'>
-          <FAQ />
-        </section>
+      <section id="faq">
+        <FAQ />
+      </section>
 
-        <section id='sponsors'>
-          <Sponsors />
-        </section>
+      <section id="sponsors">
+        <Sponsors />
+      </section>
 
-        <div className='h-[20vw] flex justify-center'>
-          <h1 className='absolute bottom-10 text-white text-[2vw] z-50'>Special thanks to HackDuke for providing the template! ❤️ </h1>
-        </div>
-      </Backdrop>
+      <div className="h-[20vw] flex justify-center">
+        <h1 className="absolute bottom-10 text-white text-[2vw] z-50">
+          Special thanks to HackDuke for providing the template! ❤️{" "}
+        </h1>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
