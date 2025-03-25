f17534e1498203c55042dda6059c3f4be61da5b3
viennguyen24 <viennguyen@usf.edu>
2025-03-25 16:29:36 -0400

    updated background images for track / about section and added usf tracks images

--- src/components/tracks.js
+++ src/components/tracks.js
@@ -1,27 +1,47 @@
-import { Panel } from "./panel"
-import { H1 } from "./typography"
+import { Panel } from "./panel";
+import { H1 } from "./typography";
 
-const Track = ({ title, image_src, image_alt, widthClass, leftClass, bottomClass, rightClass, topClass, imageWidthClass, className = "" }) => {
+const Track = ({
+  title,
+  image_src,
+  image_alt,
+  widthClass,
+  leftClass,
+  bottomClass,
+  rightClass,
+  topClass,
+  imageWidthClass,
+  className = "",
+}) => {
   // Pass class names instead of, e.g., vw units because Tailwind has issues with template literals
   return (
-    <div className={`absolute z-20 flex flex-col space-y-2 items-center ${widthClass} ${leftClass} ${bottomClass} ${rightClass} ${topClass} ${className}`}>
+    <div
+      className={`absolute z-20 flex flex-col space-y-2 items-center ${widthClass} ${leftClass} ${bottomClass} ${rightClass} ${topClass} ${className}`}
+    >
       <h3 className="text-sm md:text-3xl text-white text-center">{title}</h3>
-      <img src={image_src} alt={image_alt} className={imageWidthClass}/>
+      <img src={image_src} alt={image_alt} className={imageWidthClass} />
     </div>
-  )
-}
+  );
+};
 
 export const Tracks = () => {
   // TODO: Lighting
   // TODO: Responsiveness
   return (
-    <Panel>
+    <div
+      className={`relative w-full h-[60vw]`}
+      style={{
+        backgroundImage: `url('/image/backgrounds/tracksbackground.png')`,
+        backgroundSize: "cover",
+        backgroundPosition: "top",
+      }}
+    >
       <H1 className="text-center mb-8">Tracks</H1>
-      
+
       {/* Center Track */}
-      <Track 
+      <Track
         title="Cybersecurity"
-        image_src="/images/TracksCrab.svg"
+        image_src="/image/tracks/track1.png"
         image_alt="Crab"
         widthClass="w-[30vw]"
         imageWidthClass="w-[14vw]"
@@ -31,11 +51,11 @@ export const Tracks = () => {
         bottomClass="bottom-auto"
         className="transform -translate-x-1/2 -translate-y-1/2"
       />
-      
+
       {/* Left Top Track */}
-      <Track 
+      <Track
         title="Fintech"
-        image_src="/images/TracksTurtle.svg"
+        image_src="/image/tracks/track2.png"
         image_alt="Turtle"
         widthClass="w-[25vw]"
         imageWidthClass="w-[15vw]"
@@ -44,11 +64,11 @@ export const Tracks = () => {
         rightClass="right-auto"
         bottomClass="bottom-auto"
       />
-      
+
       {/* Right Top Track */}
       <Track
         title="Healthcare"
-        image_src="/images/TracksWhale.svg"
+        image_src="/image/tracks/track3.png"
         image_alt="Whale"
         widthClass="w-[30vw]"
         imageWidthClass="w-[22vw]"
@@ -57,24 +77,24 @@ export const Tracks = () => {
         leftClass="left-auto"
         bottomClass="bottom-auto"
       />
-      
+
       {/* Left Bottom Track */}
-      <Track 
-        title="Sustainability" 
-        image_src="/images/TracksGenericFish.svg" 
-        image_alt="Generic Fish" 
-        widthClass="w-[25vw]" 
+      <Track
+        title="Sustainability"
+        image_src="/image/tracks/track4.png"
+        image_alt="Generic Fish"
+        widthClass="w-[25vw]"
         imageWidthClass="w-[18vw]"
-        leftClass="left-[10vw]" 
-        bottomClass="bottom-[15vh]" 
-        rightClass="right-auto" 
-        topClass="top-auto" 
+        leftClass="left-[10vw]"
+        bottomClass="bottom-[15vh]"
+        rightClass="right-auto"
+        topClass="top-auto"
       />
-      
+
       {/* Right Bottom Track */}
       <Track
         title="Automation"
-        image_src="/images/Crab.png"
+        image_src="/image/tracks/track5.png"
         image_alt="Crab"
         widthClass="w-[25vw]"
         imageWidthClass="w-[18vw]"
@@ -83,6 +103,6 @@ export const Tracks = () => {
         leftClass="left-auto"
         topClass="top-auto"
       />
-    </Panel>
-  )
-}
+    </div>
+  );
+};
