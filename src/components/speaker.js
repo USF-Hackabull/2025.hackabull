630f5bd8dbdb5f7e5b8b8dcb8b3c6f178ec82545
viennguyen24 <viennguyen@usf.edu>
2025-03-26 16:49:47 -0400

    finished barebone speakers section and animation

--- src/components/speaker.js
+++ src/components/speaker.js
@@ -1,62 +1,38 @@
-import { Panel } from "./panel";
-import { H1, P } from "./typography";
+import { H1 } from "./typography";
+import { motion } from "framer-motion";
 
 export const Speaker = () => {
   return (
-    <Panel>
-      <div className="absolute z-20 w-[40vw] right-[30vw] top-[20vw] text-center" /*className="absolute z-20 w-[40vw] right-[6vw] top-[7vw] text-center"*/>
-        <H1>Speaker</H1>
-        <P>
-          Coming soon!
-        </P>
+    <div
+      className="relative w-full h-auto"
+      style={{
+        backgroundImage: `url('/image/backgrounds/speakersbackground.png')`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        aspectRatio: "16 / 9",
+      }}
+    >
+      <div
+        className="absolute z-20 -top-20 text-center w-full"
+        style={{ zIndex: 2 }}
+      >
+        <motion.img
+          src="/image/backgrounds/billboardlight.png"
+          alt="Overlay"
+          className="w-full h-auto object-contain"
+          initial={{ opacity: 1 }}
+          animate={{ opacity: [1, 0.6, 0.3, 0.8, 0.1, 1] }}
+          transition={{ duration: 1.5, repeat: Infinity, ease: "easeInOut" }}
+        />
+        <div
+          className="absolute inset-0 flex items-center justify-center z-20 flex flex-col"
+          style={{ zIndex: 2 }}
+        >
+          <H1 className="text-center mb-8">Speakers</H1>
+          <h2 className="text-3xl font-bold text-black">Coming soon</h2>
+        </div>
       </div>
-
-      {/*<div className="absolute w-[60vw] left-[-7vw] top-[-5vw]">
-        <SpeakerShark />
-      </div>*/}
-
-      <img
-        src="/images/fishy.png"
-        alt="fish"
-        className="absolute z-20 h-[15vw]"
-      />
-
-    </Panel>
-  );
-};
-
-export const SpeakerShark = () => {
-  return (
-    <div style={{ position: 'relative', width: '100%' }}>
-      <img
-        src="/images/SpeakerSharkFrame.png"
-        alt="Speaker Shark Frame"
-        style={{ width: '100%' }}
-      />
-      <img
-        src="/images/SpeakerSharkTeethOverlay.png"
-        alt="Speaker Shark Teeth"
-        style={{
-          position: 'absolute',
-          top: '39%',
-          left: '48%',
-          transform: 'translateX(-50%)',
-          width: '49%',
-          zIndex: 10,
-        }}
-      />
-      <img
-        src="/images/Speaker.png"
-        alt="Speaker"
-        style={{
-          position: 'absolute',
-          top: '43%',
-          left: '48%',
-          transform: 'translateX(-50%)',
-          width: '43%',
-          borderRadius: '50%',
-        }}
-      />
     </div>
   );
-}
+};
