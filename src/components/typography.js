d301ad025051dc284619d78676419a237edb5576
viennguyen24 <viennguyen@usf.edu>
2025-03-28 16:33:57 -0400

    updated base text and title component

--- src/components/typography.js
+++ src/components/typography.js
@@ -1,19 +1,31 @@
-const Typography = ({ as: Component = 'p', children, className, ...props }) => {
-    return (
-        <Component className={className} {...props}>
-            {children}
-        </Component>
-    );
+const Typography = ({ as: Component = "p", children, className, ...props }) => {
+  return (
+    <Component className={className} {...props}>
+      {children}
+    </Component>
+  );
 };
 
-export const H1 = ({ children, className = '', ...props }) => (
-    <Typography as="h1" className={`text-white font-bold glow text-[7vw] ${className}`} {...props}>
-        {children}
-    </Typography>
+export const Title = ({ children, className = "", ...props }) => (
+  <Typography
+    as="h1"
+    className={`text-[#C1413E] font-bold glow ${className}`}
+    style={{
+      fontSize: "clamp(1.5rem, 7vw, 5rem)",
+    }}
+    {...props}
+  >
+    {children}
+  </Typography>
 );
 
-export const P = ({ children, className = '', ...props }) => (
-    <Typography as="p" className={`text-about_ticket_text font-semibold text-[1.7vw]${className}`} {...props}>
-        {children}
-    </Typography>
+export const Text = ({ children, className = "", ...props }) => (
+  <Typography
+    as="p"
+    className={`text-[#254017] font-semibold leading-relaxed tracking-normal ${className}`}
+    style={{ fontSize: "clamp(1rem, 1.7vw, 1.4rem)" }}
+    {...props}
+  >
+    {children}
+  </Typography>
 );
